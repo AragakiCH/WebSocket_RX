0cd96df2c129c06b8e2950e4eee3e0fad2a0554a
AragakiCH <hugoaragaki.0534@gmail.com>
2026-01-19 14:01:47 -0500

    cambios_snap3

--- bin/websocket-wrapper.sh
+++ bin/websocket-wrapper.sh
@@ -6,9 +6,10 @@ export OPCUA_USER="${OPCUA_USER:-boschrexroth}"
 export OPCUA_PASSWORD="${OPCUA_PASSWORD:-boschrexroth}"
 export LOG_TO_EXCEL="${LOG_TO_EXCEL:-false}"
 
-# Port donde vive tu API dentro del snap
 export API_HOST="${API_HOST:-0.0.0.0}"
 export API_PORT="${API_PORT:-8000}"
 
-export PYTHONPATH="$SNAP/app"
-exec "$SNAP/bin/python3" -m uvicorn main:app --app-dir "$SNAP/app" --host 0.0.0.0 --port 8000
+# Tu código está en la raíz del snap: $SNAP/main.py, $SNAP/ws, etc.
+export PYTHONPATH="$SNAP"
+
+exec "$SNAP/venv/bin/python" -m uvicorn main:app --app-dir "$SNAP" --host "$API_HOST" --port "$API_PORT"
